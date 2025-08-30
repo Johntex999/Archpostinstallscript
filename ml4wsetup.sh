ff714f29448aae8379584370cceb291d0eecf105
Johntex999 <jonte.grunewald@proton.me>
2025-08-30 18:34:26 +0200

    Update ml4wsetup.sh

--- ml4wsetup.sh
+++ ml4wsetup.sh
@@ -4,14 +4,11 @@ set -e
 # Set keyboard layout to German in Hyprland
 hyprctl keyword input:kb_layout de
 
-# Install Firefox (on Arch/Manjaro)
-sudo pacman -S --noconfirm firefox
-
 # Install the dotfiles installer
 flatpak install -y flathub com.ml4w.dotfilesinstaller
 
 # Run the installer
 flatpak run com.ml4w.dotfilesinstaller &
 
-# Open Firefox at the given URL
-firefox https://mylinuxforwork.github.io/dotfiles/ &
+# Open Zen at the given URL
+zen-browser https://mylinuxforwork.github.io/dotfiles/ &
